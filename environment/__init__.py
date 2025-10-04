e43fa6d3cfd7a220b93092d1b8de6ec60bdc527e
mmostafa886 <mmostafa886@gmail.com>
2025-10-04 22:54:15 +0200

    Update the Gemini key

--- environment/__init__.py
+++ environment/__init__.py
@@ -4,4 +4,4 @@ import json
 from typing import List, Dict
 
 # Set your Google API key
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] ='/Users/mohamedsolaiman/Desktop/Python/SampleAIAgent/environment/gen-lang-client-0939087787-23012f7eb914.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] ='/Users/mohamedsolaiman/Desktop/Python/GeminKey/gen-lang-client-0939087787-dd2da9119793.json'
